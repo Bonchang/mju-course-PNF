1c861ba640665cadbcf0b59941c1e521ea559683
Bonchang <9bonbon@naver.com>
2024-11-10 21:45:04 +0900

    HW2

--- src/Components/AddFilter/AddFilter.java
+++ src/Components/AddFilter/AddFilter.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class AddFilter extends CommonFilterImpl {
-
     @Override
     public boolean specificComputationForFilter() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
@@ -18,29 +17,21 @@ public class AddFilter extends CommonFilterImpl {
         while ((line = reader.readLine()) != null) {
             String[] tokens = line.split(" ");
 
-            if (tokens.length >= 4 && tokens[3].equals("CS")) { // CS 학생인지 확인
-                boolean has12345 = false;
+            if (tokens.length >= 4 && tokens[3].equals("EE")) { // EE 학생인지 확인
                 boolean has23456 = false;
 
                 // 과목 ID 체크
                 for (int i = 4; i < tokens.length; i++) {
-                    if (tokens[i].equals("12345")) {
-                        has12345 = true;
-                    }
                     if (tokens[i].equals("23456")) {
                         has23456 = true;
+                        break;
                     }
                 }
 
                 // 과목 ID 추가
-                if (!has12345 || !has23456) {
+                if (!has23456) {
                     StringBuilder updatedLine = new StringBuilder(line);
-                    if (!has12345) {
-                        updatedLine.append(" 12345");
-                    }
-                    if (!has23456) {
-                        updatedLine.append(" 23456");
-                    }
+                    updatedLine.append(" 23456");
                     writer.write(updatedLine.toString());
                     writer.newLine();
                 } else {
--- src/Components/Middle/MiddleFilter.java
+++ src/Components/Middle/MiddleFilter.java
@@ -6,34 +6,37 @@ package Components.Middle;
 import java.io.IOException;
 import Framework.CommonFilterImpl;
 
-public class MiddleFilter extends CommonFilterImpl{
+public class MiddleFilter extends CommonFilterImpl {
     @Override
     public boolean specificComputationForFilter() throws IOException {
-    	int checkBlank = 4; 
+        int checkBlank = 4;
         int numOfBlank = 0;
         int idx = 0;
         byte[] buffer = new byte[64];
-        boolean isCS = false;    
+        boolean isEE = false;
         int byte_read = 0;
-        
-        while(true) {          
-        	// check "CS" on byte_read from student information
-            while(byte_read != '\n' && byte_read != -1) {
-            	byte_read = in.read();
-                if(byte_read == ' ') numOfBlank++;
-                if(byte_read != -1) buffer[idx++] = (byte)byte_read;
-                if(numOfBlank == checkBlank && buffer[idx-3] == 'C' && buffer[idx-2] == 'S')
-                    isCS = true;
-            }      
-            if(isCS == true) {
-                for(int i = 0; i<idx; i++) 
-                    out.write((char)buffer[i]);
-                isCS = false;
+
+        while (true) {
+            // check "EE" on byte_read from student information
+            while (byte_read != '\n' && byte_read != -1) {
+                byte_read = in.read();
+                if (byte_read == ' ') numOfBlank++;
+                if (byte_read != -1) buffer[idx++] = (byte) byte_read;
+                if (numOfBlank == checkBlank && buffer[idx - 3] == 'E' && buffer[idx - 2] == 'E') {
+                    isEE = true;
+                }
+            }
+            if (isEE) {
+                for (int i = 0; i < idx; i++) {
+                    out.write(buffer[i]);
+                }
+                out.write('\n');
+                isEE = false;
             }
             if (byte_read == -1) return true;
             idx = 0;
             numOfBlank = 0;
             byte_read = '\0';
         }
-    }  
+    }
 }
